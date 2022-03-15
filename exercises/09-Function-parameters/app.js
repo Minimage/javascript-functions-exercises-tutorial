101a9ddf3cd86fb59f044d819453d3e1894e1f0e
Minimage <seancampbell914@gmail.com>
2022-03-15 12:22:25 +0000

    09 Function parameters

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,6 +1,10 @@
 // Your code goes here:
 
+function renderPerson(p1,p2,p3,p4,p5){
+  
+  return `${p1} is a ${p4} years old ${p5} born in ${p2} with ${p3} eyes`
 
+}
 
   ///*** Do not edit below this line ****/
   console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
