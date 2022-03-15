e02a11cf1be9288ed79b25e07fc9eab54f4badec
Minimage <seancampbell914@gmail.com>
2022-03-15 02:14:30 +0000

    05 Defining vs Calling a function

--- .learn/resets/05-Defining-vs-Calling-a-function/app.js
+++ .learn/resets/05-Defining-vs-Calling-a-function/app.js
@@ -1,9 +1,5 @@
 //Define the function called "multi" that expects 2 parameters:
-function multi(x,y){
-  var holder = x*y;
-  return holder;
 
-}
 
   //don't edit anything below this line
   var returnValue = multi(7,53812212);
